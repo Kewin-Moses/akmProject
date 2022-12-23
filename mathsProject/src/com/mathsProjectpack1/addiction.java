cb9a0e2ef018e68857769e11453e7d2a11302d00
Aarochristo <aaronchristo22@gmail.com>
2022-12-23 14:14:12 +0530

    addictionpart commited

---
+++ mathsProject/src/com/mathsProjectpack1/addiction.java
@@ -0,0 +1,16 @@
+package com.mathsProjectpack1;
+
+public class addiction {
+
+	
+	
+	public void addiction()
+	{
+		
+		
+		int a=10,b=0;
+		int c=a+b;
+		
+		System.out.println("addiction printed by aaron");
+	}
+}
